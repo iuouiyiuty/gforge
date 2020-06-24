1665b01acb6d29a28aa6a9bf01357a6865e47f27
iuouiyiuty <392111422@qq.com>
2020-06-24 20:41:43 +0800

    tab

--- internal/dao/dao.go
+++ internal/dao/dao.go
@@ -9,126 +9,126 @@ import (
 const (
 	daoCode = `
 
-	func (d *Dao) {{.StructName}}DB(ctx context.Context, id int64) (*{{.StructPkg}}{{.StructName}}, error) {
-		db := d.db.Context(ctx)
+func (d *Dao) {{.StructName}}DB(ctx context.Context, id int64) (*{{.StructPkg}}{{.StructName}}, error) {
+	db := d.db.Context(ctx)
 
-		var res {{.StructPkg}}{{.StructName}}
+	var res {{.StructPkg}}{{.StructName}}
 
-		err := db.Table(res.TableName()).Where("id = ?", id).Take(&res).Error
-		if gorm.IsRecordNotFoundError(err) {
-			return nil, nil
-		}
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-	
-		return &res, nil
+	err := db.Table(res.TableName()).Where("id = ?", id).Take(&res).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 
-	func (d *Dao) {{.StructName}}GetOne(ctx context.Context, where map[string]interface{}) (*{{.StructPkg}}{{.StructName}}, error) {
-		db := d.db.Context(ctx)
+	return &res, nil
+}
 
-		var res {{.StructPkg}}{{.StructName}}
+func (d *Dao) {{.StructName}}GetOne(ctx context.Context, where map[string]interface{}) (*{{.StructPkg}}{{.StructName}}, error) {
+	db := d.db.Context(ctx)
 
-		err := db.Table(res.TableName()).Where(where).Take(&res).Error
-		if gorm.IsRecordNotFoundError(err) {
-			return nil, nil
-		}
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-	
-		return &res, nil
+	var res {{.StructPkg}}{{.StructName}}
+
+	err := db.Table(res.TableName()).Where(where).Take(&res).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 
-	func (d *Dao) {{.StructName}}List(ctx context.Context, where map[string]interface{}) ([]*{{.StructPkg}}{{.StructName}}, error) {
-		db := d.db.Context(ctx)
+	return &res, nil
+}
 
-		var res []*{{.StructPkg}}{{.StructName}}
+func (d *Dao) {{.StructName}}List(ctx context.Context, where map[string]interface{}) ([]*{{.StructPkg}}{{.StructName}}, error) {
+	db := d.db.Context(ctx)
 
-		err := db.Table({{.StructPkg}}{{.StructName}}{}.TableName()).Where(where).Find(&res).Error
-		if gorm.IsRecordNotFoundError(err) {
-			return nil, nil
-		}
-		if err != nil {
-			return nil, errors.WithStack(err)
-		}
-	
-		return res, nil
+	var res []*{{.StructPkg}}{{.StructName}}
+
+	err := db.Table({{.StructPkg}}{{.StructName}}{}.TableName()).Where(where).Find(&res).Error
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 
-	func (d *Dao) {{.StructName}}Insert(ctx context.Context, tx *gorm.DB, row *{{.StructPkg}}{{.StructName}}) (err error) {
-		if tx == nil {
-			tx = d.db.Context(ctx).Begin()
+	return res, nil
+}
+
+func (d *Dao) {{.StructName}}Insert(ctx context.Context, tx *gorm.DB, row *{{.StructPkg}}{{.StructName}}) (err error) {
+	if tx == nil {
+		tx = d.db.Context(ctx).Begin()
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
 		}
-		defer func() {
-			if r := recover(); r != nil {
-				tx.Rollback()
-			}
-			if err != nil {
-				tx.Rollback()
-			}
-			err = errors.WithStack(tx.Commit().Error)
-		}()
-	
-		if tx.NewRecord(row) {
-			err = errors.WithStack(tx.Create(row).Error)
+		if err != nil {
+			tx.Rollback()
 		}
-	
+		err = errors.WithStack(tx.Commit().Error)
+	}()
+
+	if tx.NewRecord(row) {
+		err = errors.WithStack(tx.Create(row).Error)
+	}
+
+	return
+}
+
+// 更新指定字段，有updated_at的话需要手动指定
+func (d *Dao) {{.StructName}}Update(ctx context.Context, tx *gorm.DB, where map[string]interface{}, update map[string]interface{}) (affected int64, err error) {
+	if tx == nil {
+		tx = d.db.Context(ctx).Begin()
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+		err = errors.WithStack(tx.Commit().Error)
+	}()
+
+	db := tx.Table({{.StructPkg}}{{.StructName}}{}.TableName()).Where(where).Updates(update)
+	if db.Error != nil {
+		err = errors.WithStack(db.Error)
 		return
 	}
 
-	// 更新指定字段，有updated_at的话需要手动指定
-	func (d *Dao) {{.StructName}}Update(ctx context.Context, tx *gorm.DB, where map[string]interface{}, update map[string]interface{}) (affected int64, err error) {
-		if tx == nil {
-			tx = d.db.Context(ctx).Begin()
+	affected = db.RowsAffected
+
+	return
+}
+
+// 慎用，一般都是软删除
+func (d *Dao) {{.StructName}}Delete(ctx context.Context, tx *gorm.DB, where map[string]interface{}) (affected int64, err error) {
+	if tx == nil {
+		tx = d.db.Context(ctx).Begin()
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
 		}
-		defer func() {
-			if r := recover(); r != nil {
-				tx.Rollback()
-			}
-			if err != nil {
-				tx.Rollback()
-			}
-			err = errors.WithStack(tx.Commit().Error)
-		}()
-	
-		db := tx.Table({{.StructPkg}}{{.StructName}}{}.TableName()).Where(where).Updates(update)
-		if db.Error != nil {
-			err = errors.WithStack(db.Error)
-			return
+		if err != nil {
+			tx.Rollback()
 		}
-	
-		affected = db.RowsAffected
-	
+		err = errors.WithStack(tx.Commit().Error)
+	}()
+
+	db := tx.Where(where).Delete({{.StructPkg}}{{.StructName}}{})
+	if db.Error != nil {
+		err = errors.WithStack(db.Error)
 		return
 	}
 
-	// 慎用，一般都是软删除
-	func (d *Dao) {{.StructName}}Delete(ctx context.Context, tx *gorm.DB, where map[string]interface{}) (affected int64, err error) {
-		if tx == nil {
-			tx = d.db.Context(ctx).Begin()
-		}
-		defer func() {
-			if r := recover(); r != nil {
-				tx.Rollback()
-			}
-			if err != nil {
-				tx.Rollback()
-			}
-			err = errors.WithStack(tx.Commit().Error)
-		}()
-	
-		db := tx.Where(where).Delete({{.StructPkg}}{{.StructName}}{})
-		if db.Error != nil {
-			err = errors.WithStack(db.Error)
-			return
-		}
-	
-		affected = db.RowsAffected
-	
-		return
-	}
+	affected = db.RowsAffected
+
+	return
+}
 	
 	`
 )
